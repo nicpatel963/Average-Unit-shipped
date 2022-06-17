e5ec81e4b6222b6ffbbb2c0fe9c5091138104742
Nirmal patel <51151608+nicpatel963@users.noreply.github.com>
2022-06-17 05:44:15 +0530

    Add files via upload

---
+++ Average Units Shipped/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Average_Units_Shipped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
